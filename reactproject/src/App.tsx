345e8d6218673da9f653197d1253c1bb8145e21e
amangrx <ransingaman@gmail.com>
2025-02-09 17:49:50 +0545

    using props

--- reactproject/src/App.tsx
+++ reactproject/src/App.tsx
@@ -1,9 +1,14 @@
 import ListGroup from "./components/ListGroup";
-
+  
 function App() {
+  let items = ["Pokhara", "Kathmandu", "Bhaktapur", "Lalitpur", "Biratnagar"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  }
+
   return (
     <div>
-      <ListGroup />
+      <ListGroup items={items} heading="Name of Cities" onSelectItem={handleSelectItem}/>
     </div>
   );
 }
--- reactproject/src/components/ListGroup.tsx
+++ reactproject/src/components/ListGroup.tsx
@@ -1,20 +1,32 @@
-import { MouseEvent, useState } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-  let items = ["Pokhara", "Kathmandu", "Bhaktapur", "Lalitpur", "Biratnagar"];
+interface Props {
+  items: string[];
+  heading: string;
+  onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
   const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     //<> is a shorthand for <React.Fragment> which is used to group elements without adding extra nodes to the DOM.
     <>
-      <h1>List Group</h1>
+      <h1>{heading}</h1>
       {items.length === 0 && <p>No items to show</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className={selectedIndex === index ? "list-group-item active" : "list-group-item"}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
             key={item}
-            onClick={() => {setSelectedIndex(index); }}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
